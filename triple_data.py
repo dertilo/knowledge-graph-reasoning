b85322eb48ac1f3561cafa0226b287e97f51b921
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-12-08 11:12:43 +0100

    triple-data

--- triple_data.py
+++ triple_data.py
@@ -1,17 +1,12 @@
-import math
 import random
 from dataclasses import dataclass
-from typing import Dict, List, NamedTuple, Set, Tuple
+from typing import Dict, List, Set, Tuple
 
 import torch
-import numpy as np
-from ignite.utils import convert_tensor
 from tqdm import tqdm
 from util import data_io, util_methods
 
-from kgr.conv_e import Config, ConvE
 from pytorch_util import build_BatchingDataLoader
-
 
 Trees = Dict[int, Dict[int, Set[int]]]
 Branch = Tuple[int, int, Set[int]]
@@ -59,32 +54,17 @@ def build_triple_dataset(triple_files: Dict[str, str]):
     return TripleDataset(ent2id, rel2id, ds2tr)
 
 
-def convert_to_tensors(batch: List[Branch], num_entities):
-    def convert_to_binary(batch):
-        z = torch.zeros((len(batch), num_entities))
-        for i, objs_idx in enumerate(batch):
-            z[i][list(objs_idx)] = 1
-        return z
 
-    batch_e1, batch_r, batch_e2 = [list(l) for l in zip(*batch)]
-    batch_e1 = torch.LongTensor(batch_e1)
-    batch_r = torch.LongTensor(batch_r)
-    batch_e2 = convert_to_binary(batch_e2)
-
-    return batch_e1, batch_e2, batch_r
-
-
-def build_batch_iter(trees: Trees, num_ents, batch_size=32):
+def build_batch_iter(trees: Trees, batch_size=32):
 
     branches: List[Branch] = [
         (e1, r, e2s) for e1, r_to_e2s in trees.items() for r, e2s in r_to_e2s.items()
     ]
     random.shuffle(branches)
-    g = (
-        convert_to_tensors(raw_batch, num_ents)
+    return iter(
+        raw_batch
         for raw_batch in util_methods.iterable_to_batches(branches, batch_size)
     )
-    return iter(g)
 
 
 def build_resetting_next_fun(iter_supplier):
@@ -111,15 +91,11 @@ if __name__ == "__main__":
 
     num_entities = len(data.ent2id.keys())
     get_batch_fun = build_resetting_next_fun(
-        lambda: build_batch_iter(data.dataset2trees["dev"], num_entities, 32)
+        lambda: build_batch_iter(data.dataset2trees["dev"], 32)
     )
 
     dl = build_BatchingDataLoader(get_batch_fun=get_batch_fun)
 
-    num_relations = len(data.rel2id.keys())
-    config = Config()
-    model = ConvE(config, num_entities, num_relations)
-
     for epoch in range(3):
-        for batch in tqdm(dl):
-            batch = convert_tensor(list(batch))
+        for raw_batch in tqdm(dl):
+            raw_batch
