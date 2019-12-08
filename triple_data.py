e6f918e030d180c75ddc8adb36ef2135a2a09b66
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-12-08 10:50:31 +0100

    Trees, Branch

--- triple_data.py
+++ triple_data.py
@@ -1,27 +1,27 @@
 import math
 import random
 from dataclasses import dataclass
-from typing import Dict, List, NamedTuple
+from typing import Dict, List, NamedTuple, Set, Tuple
 
 import torch
 import numpy as np
+from ignite.utils import convert_tensor
 from tqdm import tqdm
 from util import data_io, util_methods
 
+from kgr.conv_e import Config, ConvE
 from pytorch_util import build_BatchingDataLoader
 
 
-class Triple(NamedTuple):
-    subject: int
-    predicate: int
-    object: int
+Trees = Dict[int, Dict[int, Set[int]]]
+Branch = Tuple[int, int, Set[int]]
 
 
 @dataclass
 class TripleDataset:
     ent2id: Dict[str, int]
     rel2id: Dict[str, int]
-    dataset2triples: Dict[str, List[Triple]]
+    dataset2trees: Dict[str, Trees]
 
 
 def get_id(x2id: Dict, x):
@@ -33,13 +33,24 @@ def get_id(x2id: Dict, x):
 def build_triple_dataset(triple_files: Dict[str, str]):
     ent2id, rel2id = {}, {}
 
-    def build_triples(file):
-        def process_line(line):
+    def build_triples(file) -> Trees:
+        trees = {}
+
+        def add_triple(subj, predi, obje, triple_dict: Trees):
+            if subj not in triple_dict:
+                triple_dict[subj] = {}
+            if predi not in triple_dict[subj]:
+                triple_dict[subj][predi] = set()
+            triple_dict[subj][predi].add(obje)
+
+        for line in data_io.read_lines(file):
             s, o, p = line.strip().split("\t")
             s_id, o_id, p_id = get_id(ent2id, s), get_id(ent2id, o), get_id(rel2id, p)
-            return Triple(s_id, p_id, o_id)
+            p_inv_id = get_id(rel2id, p + "_inv")
+            add_triple(s_id, p_id, o_id, trees)
+            add_triple(o_id, p_inv_id, s_id, trees)
 
-        return [process_line(line) for line in data_io.read_lines(file)]
+        return trees
 
     ds2tr = {
         dataset_name: build_triples(triple_file)
@@ -48,44 +59,30 @@ def build_triple_dataset(triple_files: Dict[str, str]):
     return TripleDataset(ent2id, rel2id, ds2tr)
 
 
-def rand_ent_except(num_ent, ent):
-    rand_ent = random.randint(0, num_ent - 1)
-    while rand_ent == ent:
-        rand_ent = random.randint(0, num_ent - 1)
-    return rand_ent
+def convert_to_tensors(batch: List[Branch], num_entities):
+    def convert_to_binary(batch):
+        z = torch.zeros((len(batch), num_entities))
+        for i, objs_idx in enumerate(batch):
+            z[i][list(objs_idx)] = 1
+        return z
+
+    batch_e1, batch_r, batch_e2 = [list(l) for l in zip(*batch)]
+    batch_e1 = torch.LongTensor(batch_e1)
+    batch_r = torch.LongTensor(batch_r)
+    batch_e2 = convert_to_binary(batch_e2)
+
+    return batch_e1, batch_e2, batch_r
 
 
-def build_batch(raw_batch: List[Triple], num_ents, neg_ratio=1):
-    # TODO(tilo): neg_ratio ??
+def build_batch_iter(trees: Trees, num_ents, batch_size=32):
 
-    pos_batch = np.array([[tr.subject, tr.predicate, tr.object] for tr in raw_batch])
-
-    neg_batch = np.repeat(np.copy(pos_batch), neg_ratio, axis=0)
-    for i in range(len(neg_batch)):
-        if random.random() < 0.5:
-            neg_batch[i][0] = rand_ent_except(
-                num_ents, neg_batch[i][0]
-            )  # flipping head
-        else:
-            neg_batch[i][2] = rand_ent_except(
-                num_ents, neg_batch[i][2]
-            )  # flipping tail
-
-    input_batch = np.append(pos_batch, neg_batch, axis=0)
-    neg_targets = np.zeros((neg_batch.shape[0], 1))  # zeros or -1 ??
-    pos_targets = np.ones((pos_batch.shape[0], 1))
-    target_batch = np.concatenate([pos_targets, neg_targets])
-
-    input_tensor = torch.tensor(input_batch).long()
-    target_tensor = torch.tensor(target_batch).float()
-    return input_tensor, target_tensor
-
-
-def build_batch_iter(triples: List[Triple], num_ents, batch_size=32):
-    random.shuffle(triples)
+    branches: List[Branch] = [
+        (e1, r, e2s) for e1, r_to_e2s in trees.items() for r, e2s in r_to_e2s.items()
+    ]
+    random.shuffle(branches)
     g = (
-        build_batch(raw_batch, num_ents)
-        for raw_batch in util_methods.iterable_to_batches(triples, batch_size)
+        convert_to_tensors(raw_batch, num_ents)
+        for raw_batch in util_methods.iterable_to_batches(branches, batch_size)
     )
     return iter(g)
 
@@ -112,14 +109,17 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    num_entities = len(data.ent2id.keys())
     get_batch_fun = build_resetting_next_fun(
-        lambda: build_batch_iter(
-            data.dataset2triples["dev"], len(data.ent2id.keys()), 32
-        )
+        lambda: build_batch_iter(data.dataset2trees["dev"], num_entities, 32)
     )
 
     dl = build_BatchingDataLoader(get_batch_fun=get_batch_fun)
 
+    num_relations = len(data.rel2id.keys())
+    config = Config()
+    model = ConvE(config, num_entities, num_relations)
+
     for epoch in range(3):
         for batch in tqdm(dl):
-            pass
+            batch = convert_tensor(list(batch))
